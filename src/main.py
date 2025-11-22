7213971d141c07b8e7332cb59bfeb0baca106d7d
LouisB <louis.barboutie@gmail.com>
2025-11-22 10:24:54 +0100

    Add type hinting

--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ import json
 import logging
 from erfa import ErfaWarning
 import warnings
+from typing import List, Tuple
 
 pio.renderers.default = "browser"
 from jplephem.calendar import compute_julian_date
@@ -30,7 +31,7 @@ from astropy.time import Time, TimeDelta
 from astropy.coordinates import CartesianDifferential, CartesianRepresentation
 from poliastro.plotting.interactive import OrbitPlotter3D
 from poliastro import iod
-from poliastro.bodies import Earth, Mars, Sun
+from poliastro.bodies import Body, Earth, Mars, Sun
 from poliastro.ephem import Ephem
 from poliastro.maneuver import Maneuver
 from poliastro.twobody import Orbit
@@ -44,7 +45,7 @@ import plotly.io as pio
 ########################################################################################################################
 
 
-def get_data(url, year):
+def get_data(url: str, year: int) -> List[float] | None:
     element = [0, 0, 0, 0, 0, 0]
     response = requests.get(url)
     raw_data = response.json()["result"]
@@ -90,15 +91,11 @@ def get_data(url, year):
 
                 if str(year) in lines[j]:
                     count += 1
-
-                # logging.info(f'j: {j}')
-                # logging.info(lines[j])
-                # logging.info(element)
             else:
                 return element
 
 
-def get_elements(url):
+def get_elements(url: str) -> None:
     # Acquiring and Storing the data as a json file
     response = requests.get(url)
     data = response.json()
@@ -130,29 +127,29 @@ def get_elements(url):
         logging.info("Pattern not found.")
 
 
-def normalize(v):
+def normalize(v: np.ndarray) -> np.ndarray:
     norm = np.sqrt(v[0] ** 2 + v[1] ** 2 + v[2] ** 2)
     v_norm = v / norm
     return v_norm
 
 
-def length(v):
+def length(v: np.ndarray) -> float:
     v = np.sqrt(v[0] ** 2 + v[1] ** 2 + v[2] ** 2)
     return v
 
 
-def node_line(x, y):
+def node_line(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     return np.cross(x, y)
 
 
-def eccentricity_vector(mu, r, v):
+def eccentricity_vector(mu: float, r: np.ndarray, v: np.ndarray) -> np.ndarray:
     rl = length(r)
     vl = length(v)
     e = 1 / mu * ((vl**2 - mu / rl) * r - (np.dot(r, v)) * v)
     return e
 
 
-def radius_from_elements_pqw(a, e, nu):
+def radius_from_elements_pqw(a: float, e: float, nu: float) -> List[float]:
     r = a * (1 - pow(e, 2)) / (1 + e * np.cos(nu))
     p = r * np.cos(nu)
     q = r * np.sin(nu)
@@ -160,7 +157,7 @@ def radius_from_elements_pqw(a, e, nu):
     return [p, q, w]
 
 
-def velocity_from_elements_pqw(a, e, nu, mu):
+def velocity_from_elements_pqw(a: float, e: float, nu: float, mu: float) -> List[float]:
     semi_latus_rectum = a * (1 - pow(e, 2))
     r = np.sqrt(mu / semi_latus_rectum)
     p = -r * np.sin(nu)
@@ -169,7 +166,7 @@ def velocity_from_elements_pqw(a, e, nu, mu):
     return [p, q, w]
 
 
-def rv_calculation(elements):
+def rv_calculation(elements: List[float]) -> Tuple[np.ndarray, np.ndarray]:
     r = radius_from_elements_pqw(elements[0], elements[1], elements[6]) * u.km
     v = (
         velocity_from_elements_pqw(elements[0], elements[1], elements[6], mu_sun)
@@ -179,7 +176,7 @@ def rv_calculation(elements):
     return r, v
 
 
-def define_orbit(origin, classical_elements, epoch):
+def define_orbit(origin: Body, classical_elements: List[float], epoch: Time):
     orb = Orbit.from_classical(
         origin,
         classical_elements[0] * u.km,
@@ -193,7 +190,7 @@ def define_orbit(origin, classical_elements, epoch):
     return orb
 
 
-def define_ephem(origin, classical_elements):
+def define_ephem(origin: Body, classical_elements: List[float]):
     orb = Ephem.from_orbit(
         origin,
         classical_elements[0] * u.km,
@@ -206,22 +203,22 @@ def define_ephem(origin, classical_elements):
     return orb
 
 
-def sphere_of_influence(m1, r):
+def sphere_of_influence(m1: float, r: float) -> float:
     mu = 1.32712440042e11  # mu of sun
-    return r * pow(m1 / mu, 2 / 5)
+    return r * (m1 / mu) ** (2 / 5)
 
 
-def angular_momentum_normalized(i, raan):
+def angular_momentum_normalized(i: float, raan: float) -> np.ndarray:
     h = [np.sin(i) * np.sin(raan), -np.sin(i) * np.cos(raan), np.cos(i)]
-    return h
+    return np.array(h)
 
 
-def nu_to_eccentric_anomaly(e, nu):
+def nu_to_eccentric_anomaly(e: float, nu: float) -> float:
     E = 2 * np.arctan(np.tan(nu / 2) * np.sqrt((1 - e) / (1 + e)))
     return E
 
 
-def time_to_node_line(a, e, w, nu, mu):
+def time_to_node_line(a: float, e: float, w: float, nu: float, mu: float):
     # nu corresponds to current true anomaly
     E0 = nu_to_eccentric_anomaly(e, nu)  # Initial E
     E_asc = nu_to_eccentric_anomaly(e, 2 * np.pi - w)  # Final E
@@ -239,7 +236,7 @@ def time_to_node_line(a, e, w, nu, mu):
     return dt_asc, dt_des
 
 
-def julian_to_current(jd):
+def julian_to_current(jd: float):
     j = jd + 0.5
     z = floor(j)
     alpha = floor((z - 1867216.25) / 36524.25)
@@ -262,7 +259,7 @@ def julian_to_current(jd):
     return date_str
 
 
-def argument_perigee(n, e):
+def argument_perigee(n: np.ndarray, e: np.ndarray) -> float:
     nl = length(n)
     el = length(e)
     w = np.arccos(np.dot(n, e) / (el * nl))
@@ -273,7 +270,7 @@ def argument_perigee(n, e):
         return w
 
 
-def angular_momentum(r, v):
+def angular_momentum(r: np.ndarray, v: np.ndarray):
     return np.cross(r, v)
 
 
